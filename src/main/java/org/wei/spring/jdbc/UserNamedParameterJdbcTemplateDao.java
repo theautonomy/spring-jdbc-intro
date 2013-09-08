c5da7d8d42ab5499f7224ad12262b84b17ba29be
weili <weili.mail@gmail.com>
2013-09-07 20:19:38 -0400

    Remove unneeded import

--- src/main/java/org/wei/spring/jdbc/UserNamedParameterJdbcTemplateDao.java
+++ src/main/java/org/wei/spring/jdbc/UserNamedParameterJdbcTemplateDao.java
@@ -9,7 +9,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
