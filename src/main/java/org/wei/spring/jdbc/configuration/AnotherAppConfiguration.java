993559368a90043deba4d8fdc5fa67ca455858e5
weili <weili.mail@gmail.com>
2013-09-22 11:26:59 -0400

    Delete a branch

--- src/main/java/org/wei/spring/jdbc/configuration/AnotherAppConfiguration.java
+++ src/main/java/org/wei/spring/jdbc/configuration/AnotherAppConfiguration.java
@@ -1,19 +1,12 @@
 package org.wei.spring.jdbc.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.wei.spring.jdbc.domain.User;
 
 @Configuration
 public class AnotherAppConfiguration {
-	
-	@Autowired
-	@Qualifier("userFactory")
-	private UserFactoryBean userFactoryBean;
 
-	/*
 	@Bean
 	public User user() {
 		User user =  new User();
@@ -21,18 +14,4 @@ public class AnotherAppConfiguration {
 		user.setPin(4321);
 		return user;
 	}
-	*/
-	
-	/*
-	@Bean
-	public UserFactoryBean userFactoryBean() {
-		return new UserFactoryBean();
-	}
-	*/
-	
-	@Bean(name="myUser")
-	public User user() throws Exception {
-		return userFactoryBean.getObject();
-	}
-	
 }
