446a7603c4b140642e5c4fccc2994ba29d38edfc
weili <weili.mail@gmail.com>
2013-09-22 09:06:32 -0400

    Add 2nd application java configuration class for test purpose.

---
+++ src/main/java/org/wei/spring/jdbc/configuration/AnotherAppConfiguration.java
@@ -0,0 +1,17 @@
+package org.wei.spring.jdbc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.wei.spring.jdbc.domain.User;
+
+@Configuration
+public class AnotherAppConfiguration {
+
+	@Bean
+	public User user() {
+		User user =  new User();
+		user.setName("wei");
+		user.setPin(4321);
+		return user;
+	}
+}
